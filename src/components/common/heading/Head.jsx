e0f0189a894429341eb81055e50c24bd176bd9f8
dami77del <ddeligdisch@gmail.com>
2023-09-18 00:23:36 +0200

    first commit

---
+++ src/components/common/heading/Head.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Head() {
+    return (
+        <>
+            <section className='head'>
+                <div className="container flexSB">
+                    <div className='logo'>
+                        <h1>ACADEMIA</h1>
+                        <span>ONLINE EDUCATION & LEARNING</span>
+                    </div>
+                   <div className="social">
+                    <i className="fa fa-facebook-f"></i>
+                    <i className="fa fa-instagram"></i>
+                   </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Head
---
+++ src/components/common/heading/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from './Head'
+
+function Header() {
+  return (
+    <>
+<Head/>
+    </>
+  )
+}
+
+export default Header
